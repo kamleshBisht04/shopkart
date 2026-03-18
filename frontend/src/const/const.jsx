95642849a821a96600a390665d1aaf5ff883aaf3
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-03-19 01:32:55 +0530

    adding the clerk interface for login

---
+++ frontend/src/const/const.jsx
@@ -0,0 +1,6 @@
+export const Menu = [
+  { id: 1, name: "Home", link: "/" },
+  { id: 2, name: "Products", link: "/products" },
+  { id: 3, name: "About", link: "/about" },
+  { id: 4, name: "Contact", link: "/contact" },
+];
--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,17 +1,12 @@
 import logo from "@/assets/logo.png";
 import { MapPin, Search } from "lucide-react";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaCaretDown, FaShoppingCart } from "react-icons/fa";
 import { MdOutlineClose } from "react-icons/md";
 import { CgMenuLeft } from "react-icons/cg";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useState } from "react";
-
-const Menu = [
-  { id: 1, name: "Home", link: "/" },
-  { id: 2, name: "Products", link: "/products" },
-  { id: 3, name: "About", link: "/about" },
-  { id: 4, name: "Contact", link: "/contact" },
-];
+import { Menu } from "@/const/const";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -19,36 +14,35 @@ const Navbar = () => {
   return (
     <div className="relative z-50 bg-white shadow-md duration-200">
       {/* Upper Navbar */}
-      <div className="bg-primary/10">
+      <div className="bg-primary/10 py-1">
         <div className="container-custom flex items-center justify-between">
           <div className="flex gap-10">
-            <Link
-              to="/"
-              className="text-tertiary flex items-center text-2xl font-semibold sm:text-3xl"
-            >
-              <img src={logo} alt="Zeptro website logo" className="h-11 w-11" />
-              <span className="font-serif text-red-500">Z</span>aptro
-            </Link>
-            <div className="flex cursor-pointer items-center gap-1">
-              <MapPin />
-              <span className="font-semibold">
+            <NavLink to="/" className="flex items-center text-2xl sm:text-3xl">
+              <img src={logo} alt="Zeptro website logo" className="h-12 w-10" />
+              <span className="text-danger ml-2 font-serif font-bold">Z</span>
+              <span className="text-tertiary font-bold">aptro</span>
+            </NavLink>
+            <div className="hidden cursor-pointer items-center gap-1 sm:flex">
+              <MapPin className="text-danger" />
+              <span className="text-tertiary font-semibold">
                 {location ? <div></div> : "Add Address"}
               </span>
+              <FaCaretDown size={18} />
             </div>
           </div>
 
           {/* Right side  */}
-          <div className="flex items-center gap-5">
+          <div className="flex items-center gap-10">
             <div className="group relative hidden sm:block">
               <input
                 type="text"
                 placeholder="Search"
-                className="focus:border-primary w-[200px] rounded-full border border-gray-300 px-3 py-1 transition-all duration-300 group-hover:w-[300px] focus:outline-none"
+                className="focus:border-primary border-tertiary w-[200px] rounded-full border px-3 py-1 transition-all duration-300 group-hover:w-[300px] placeholder:text-gray-800 focus:outline-none"
               />
-              <Search className="group-hover:text-primary absolute top-1/2 right-3 -translate-y-1/2 text-gray-500" />
+              <Search className="group-hover:text-text-primary absolute top-1/2 right-3 -translate-y-1/2 text-gray-500" />
             </div>
             {/* order button */}
-            <button className="from-primary to-secondary flex items-center gap-2 rounded-full bg-gradient-to-r px-4 py-1 text-white">
+            <button className="bg-primary-gradient flex items-center gap-2 rounded-full px-4 py-1 text-white">
               <span className="hidden sm:block">Order</span>
               <div className="relative">
                 <FaShoppingCart className="relative text-lg" />
@@ -60,15 +54,26 @@ const Navbar = () => {
             {/* Dark Mode */}
             {/* <DarkMode/> */}
 
+            {/* signIn */}
+
+            <div className="hidden sm:block">
+              <Show when="signed-out">
+                <SignInButton className="bg-danger font-semibold cursor-pointer rounded-md px-3 py-1 text-white" />
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </div>
+
             {/* Burger Icon (Mobile) */}
             <button
-              className="sm:hidden"
+              className="mr-3 sm:hidden"
               onClick={() => setIsMobileMenuOpen((prev) => !prev)}
             >
               {isMobileMenuOpen ? (
-                <CgMenuLeft size={22} />
-              ) : (
                 <MdOutlineClose size={22} />
+              ) : (
+                <CgMenuLeft size={22} />
               )}
             </button>
           </div>
@@ -78,10 +83,15 @@ const Navbar = () => {
       <div className="bg-background hidden justify-center sm:flex">
         <ul className="flex items-center gap-16 py-3">
           {Menu.map((item) => (
-            <li>
-              <Link className="hover:text-primary duration-200">
+            <li key={item.id}>
+              <NavLink
+                to={item.link}
+                className={({ isActive }) =>
+                  `text-md relative cursor-pointer pb-1 ${isActive ? "text-tertiary font-semibold" : "text-black"} after:bg-primary after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:transition-transform after:duration-300 after:content-[''] ${isActive ? "after:scale-x-100" : ""}`
+                }
+              >
                 {item.name}
-              </Link>
+              </NavLink>
             </li>
           ))}
         </ul>
@@ -92,13 +102,13 @@ const Navbar = () => {
           <ul className="flex flex-col gap-4 p-4">
             {Menu.map((item) => (
               <li key={item.id}>
-                <Link
+                <NavLink
                   to={item.link}
                   onClick={() => setIsMobileMenuOpen(false)}
                   className="block"
                 >
                   {item.name}
-                </Link>
+                </NavLink>
               </li>
             ))}
           </ul>
